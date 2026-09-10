23a76ccda3c1085bc31c844282399201f970c933
我叫石头鱼 <sty2985707459@gmail.com>
2026-09-10 16:51:35 +0800

    feat: 更换 icon

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(desktop)");
     println!("cargo::rustc-check-cfg=cfg(mobile)");
+    println!("cargo::rerun-if-changed=icons");
     tauri_build::build()
 }
